150293a65b29b31575580a86b231ed920144cad7
Jared Thirsk (Ahatem) <j.commits@thirsk.ca>
2013-12-23 15:29:35 -0700

    changed updater exe file name

--- src/NAppUpdate.Framework/UpdateManager.cs
+++ src/NAppUpdate.Framework/UpdateManager.cs
@@ -33,7 +33,8 @@ namespace AppUpdate
 						{
 							TempFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString()),
 							UpdateProcessName = "NAppUpdateProcess",
-							UpdateExecutableName = "foo.exe", // Naming it updater.exe seem to trigger the UAC, and we don't want that
+                            //UpdateExecutableName = "foo.exe", // Naming it updater.exe seem to trigger the UAC, and we don't want that
+                            UpdateExecutableName = "SelfUpdater.exe", // Naming it updater.exe seem to trigger the UAC, and we don't want that
 						};
 
 			// Need to do this manually here because the BackupFolder property is protected using the static instance, which we are
